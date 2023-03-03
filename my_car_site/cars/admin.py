c9c15044206ec92c608ea950102686db8f6437b1
Mikhail Bobrov <BOBRTVLOL@gmail.com>
2023-03-03 19:01:25 +0100

    Formatted car model for admin panel.

--- my_car_site/cars/admin.py
+++ my_car_site/cars/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cars.models import Car
+
 
 # Register your models here.
+class CarAdmin(admin.ModelAdmin):
+    #fields = ['year', 'brand']
+    fieldsets = [
+        ('DATE INFO: ', {'fields': ['year']}),
+        ('CAR INFO', {'fields': ['brand']})
+    ]
+
+
+admin.site.register(Car, CarAdmin)
